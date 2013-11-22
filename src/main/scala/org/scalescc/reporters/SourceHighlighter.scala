9c18b005f2f4557cc9ad6b1e0cb9515501efcd2d
sksamuel <sam@sksamuel.com>
2013-11-22 14:22:48 +0000

    Added HTML scaffolding for source file page and fixed escaping on spaces.

--- src/main/scala/org/scalescc/reporters/SourceHighlighter.scala
+++ src/main/scala/org/scalescc/reporters/SourceHighlighter.scala
@@ -4,7 +4,7 @@ import java.io.{FileReader, File}
 import org.apache.commons.io.IOUtils
 import scales._
 import scala.collection.JavaConverters._
-import scala.xml.Node
+import scala.xml.{Unparsed, Node}
 import scales.MeasuredStatement
 import scales.MeasuredFile
 
@@ -19,19 +19,28 @@ class SourceHighlighter {
 
   def print(lines: Seq[String], statements: Iterable[MeasuredStatement]): Node = {
     var lineNumber = 0
-    <table>
-      {lines.map(_.replace(" ", "&nbsp")).map(line => {
-      lineNumber = lineNumber + 1
-      <tr>
-        <td>
-          {lineNumber.toString}
-        </td>
-        <td>
-          {line}
-        </td>
-      </tr>
-    })}
-    </table>
+    <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title id='title'>Scales Code Coverage</title>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css"/>
+      </head>
+      <body>
+        <table>
+          {lines.map(_.replace(" ", "&nbsp")).map(line => {
+          lineNumber = lineNumber + 1
+          <tr>
+            <td>
+              {lineNumber.toString}
+            </td>
+            <td>
+              {Unparsed(line)}
+            </td>
+          </tr>
+        })}
+        </table>
+      </body>
+    </html>
   }
 
   def lineCss(status: LineStatus): String = status match {
