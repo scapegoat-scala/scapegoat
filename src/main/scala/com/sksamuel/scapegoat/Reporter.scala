b05ddfe0688e1656f258b416ea1aafdbf17c16ef
sksamuel <sam@sksamuel.com>
2014-07-13 23:10:05 +0100

    ADded warning domain obj

--- src/main/scala/com/sksamuel/scapegoat/Reporter.scala
+++ src/main/scala/com/sksamuel/scapegoat/Reporter.scala
@@ -4,8 +4,10 @@ import scala.collection.mutable.ListBuffer
 
 /** @author Stephen Samuel */
 class Reporter {
-  val warnings = new ListBuffer[String]
-  def warn(text: String): Unit = {
-    warnings.append(text)
+  val warnings = new ListBuffer[Warning]
+  def warn(text: String, line: Int): Unit = {
+    warnings.append(Warning(text, line))
   }
 }
+
+case class Warning(text: String, line: Int)
