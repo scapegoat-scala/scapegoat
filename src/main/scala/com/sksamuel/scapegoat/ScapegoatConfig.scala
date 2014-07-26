8e87604da42c038021580f5a125ad6ba211b0960
sksamuel <sam@sksamuel.com>
2014-07-26 17:17:34 +0100

    Disabling config library use until output of stack overflow question

--- src/main/scala/com/sksamuel/scapegoat/ScapegoatConfig.scala
+++ src/main/scala/com/sksamuel/scapegoat/ScapegoatConfig.scala
@@ -1,17 +1,26 @@
 package com.sksamuel.scapegoat
 
-import com.typesafe.config.ConfigFactory
-
-import scala.collection.JavaConverters._
+import com.sksamuel.scapegoat.inspections._
 
 /** @author Stephen Samuel */
 object ScapegoatConfig extends App {
-  def inspections: Seq[Inspection] = {
-    def names: Seq[String] = {
-      val conf = ConfigFactory.load()
-      conf.getObject("scapegoat").keySet.asScala.toSeq
-    }
-    def instance(name: String): Inspection = Class.forName(name).newInstance.asInstanceOf[Inspection]
-    names.map(instance)
-  }
+  def inspections: Seq[Inspection] = Seq(new AsInstanceOf,
+    new CatchNpe,
+    new ComparisonWithSelf,
+    new ComparingFloatingPointTypes,
+    new EmptyElseBlock,
+    new EmptyFinallyBlock,
+    new JavaConversionsUse,
+    new NullUse,
+    new OptionGet,
+    new ReturnUse,
+    new UnusedMethodParameter,
+    new VarUse)
+  //    def names: Seq[String] = {
+  //      val conf = ConfigFactory.load()
+  //      conf.getObject("scapegoat").keySet.asScala.toSeq
+  //    }
+  //    def instance(name: String): Inspection = Class.forName(name).newInstance.asInstanceOf[Inspection]
+  //    names.map(instance)
+  //  }
 }
