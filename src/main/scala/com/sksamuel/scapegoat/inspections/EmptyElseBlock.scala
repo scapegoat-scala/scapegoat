aa459e451f2697dc78af865622c2e6b5ad5dbe74
sksamuel <sam@sksamuel.com>
2014-07-26 06:26:13 +0100

    Added empty else block test

---
+++ src/main/scala/com/sksamuel/scapegoat/inspections/EmptyElseBlock.scala
@@ -0,0 +1,19 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Inspection, Reporter}
+
+import scala.reflect.runtime._
+
+/** @author Stephen Samuel */
+class EmptyElseBlock extends Inspection {
+  override def traverser(reporter: Reporter) = new universe.Traverser {
+    override def traverse(tree: scala.reflect.runtime.universe.Tree): Unit = {
+      tree match {
+        case universe.If(cond, thenp, elsep) =>
+          if (elsep.children.isEmpty)
+            reporter.warn("Empty else block", tree.pos.line, "Empty else block " + cond.toString().take(100))
+        case _ => super.traverse(tree)
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/EmptyElseBlockTest.scala
@@ -0,0 +1,32 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers}
+
+/** @author Stephen Samuel */
+class EmptyElseBlockTest extends FreeSpec with ASTSugar with Matchers with PluginRunner {
+
+  override val inspections = Seq(new EmptyElseBlock)
+
+  "ComparisonWithSelf" - {
+    "should report warning" in {
+
+      val code = """object Test {
+                      if (true) {
+                        println("sammy")
+                      } else {
+                      }
+
+                      if (true) {
+                        println("sammy")
+                      } else {
+                        println("snakes")
+                      }
+
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.reporter.warnings.size shouldBe 1
+    }
+  }
+}
