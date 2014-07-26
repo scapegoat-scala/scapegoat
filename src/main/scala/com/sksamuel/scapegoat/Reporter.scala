2510a0d87011f1cc7a7f850a0020db103c82db2d
sksamuel <sam@sksamuel.com>
2014-07-26 18:02:36 +0100

    Added normalized path

--- src/main/scala/com/sksamuel/scapegoat/Reporter.scala
+++ src/main/scala/com/sksamuel/scapegoat/Reporter.scala
@@ -12,7 +12,10 @@ class Reporter {
     warn(text, tree, level, Option(snippet))
   }
   def warn(text: String, tree: scala.reflect.runtime.universe.Tree, level: Level, snippet: Option[String]): Unit = {
-    warnings.append(Warning(text, tree.pos.line, level, tree.pos.source.file.path, snippet))
+    val sourceFile = tree.pos.source.file.path
+    val indexOf = sourceFile.indexOf("src/main/scala")
+    val normalizedPath = if (indexOf == -1) sourceFile else sourceFile.drop(indexOf)
+    warnings.append(Warning(text, tree.pos.line, level, normalizedPath, snippet))
   }
   def warnings(level: Level): Seq[Warning] = warnings.filter(_.level == level)
 }
