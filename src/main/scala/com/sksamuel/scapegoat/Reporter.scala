47036eae9ebae7f7790e73e7cc37fcfc7473cbdb
sksamuel <sam@sksamuel.com>
2014-07-27 09:29:12 +0100

    Fixed normalized path

--- src/main/scala/com/sksamuel/scapegoat/Reporter.scala
+++ src/main/scala/com/sksamuel/scapegoat/Reporter.scala
@@ -14,7 +14,7 @@ class Reporter {
   def warn(text: String, tree: scala.reflect.runtime.universe.Tree, level: Level, snippet: Option[String]): Unit = {
     val sourceFile = tree.pos.source.file.path
     val indexOf = sourceFile.indexOf("src/main/scala")
-    val normalizedPath = if (indexOf == -1) sourceFile else sourceFile.drop(indexOf)
+    val normalizedPath = if (indexOf == -1) sourceFile else sourceFile.drop(indexOf).drop("src/main/scala".length)
     warnings.append(Warning(text, tree.pos.line, level, normalizedPath, snippet))
   }
   def warnings(level: Level): Seq[Warning] = warnings.filter(_.level == level)
