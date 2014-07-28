0930855f853362f63bf8f9d3c9c7421bd17be8aa
sksamuel <sam@sksamuel.com>
2014-07-28 13:36:04 +0100

    #3 Improved broken oddness

--- src/main/scala/com/sksamuel/scapegoat/inspections/BrokenOddness.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/BrokenOddness.scala
@@ -4,7 +4,10 @@ import com.sksamuel.scapegoat.{Levels, Inspection, Reporter}
 
 import scala.reflect.runtime._
 
-/** @author Stephen Samuel */
+/** @author Stephen Samuel
+  *
+  *         Inspired by http://codenarc.sourceforge.net/codenarc-rules-basic.html#BrokenOddnessCheck
+  **/
 class BrokenOddness extends Inspection {
 
   override def traverser(reporter: Reporter) = new universe.Traverser {
@@ -16,7 +19,8 @@ class BrokenOddness extends Inspection {
         case Apply(Select(Apply(Select(_, TermName("$percent")), List(Literal(Constant(2)))),
         TermName("$eq$eq")), List(Literal(Constant(1)))) =>
           reporter.warn("Broken odd check", tree, level = Levels.Warning,
-            "Broken odd check near " + tree.toString().take(100))
+            "Potentially broken odd check. " + tree.toString().take(500) + "." +
+              "Code that attempts to check for oddness using x % 2 == 1 will fail on negative numbers. Consider using x % 2 != 0")
         case _ => super.traverse(tree)
       }
     }
