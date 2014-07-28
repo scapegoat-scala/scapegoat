aef2adad49ba2283ebe2ed62cbc0264355e62d3d
sksamuel <sam@sksamuel.com>
2014-07-28 06:50:58 +0100

    Added snippet for null

--- src/main/scala/com/sksamuel/scapegoat/inspections/NullUse.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/NullUse.scala
@@ -11,7 +11,8 @@ class NullUse extends Inspection {
   override def traverser(reporter: Reporter) = new universe.Traverser {
     override def traverse(tree: scala.reflect.runtime.universe.Tree): Unit = {
       tree match {
-        case Literal(Constant(null)) => reporter.warn("null use", tree, level = Levels.Error)
+        case Literal(Constant(null)) => reporter.warn("null use", tree, Levels.Error,
+          "null used near: " + tree.toString().take(300))
         case _ => super.traverse(tree)
       }
     }
