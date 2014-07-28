64f3fdcfdd873e1c0963f46f50d3fe8e10664ac2
sksamuel <sam@sksamuel.com>
2014-07-28 06:50:35 +0100

    Updated styles

--- src/main/scala/com/sksamuel/scapegoat/io/HtmlReportWriter.scala
+++ src/main/scala/com/sksamuel/scapegoat/io/HtmlReportWriter.scala
@@ -13,14 +13,17 @@ object HtmlReportWriter {
       |        font-family: 'Ubuntu', sans-serif;
       |        padding: 0 15px;
       |      }
+      |
       |      h1 {
       |      	color: #515151;
       |      	font-weight: 700;
       |      }
+      |
       |      h3 {
       |      	color: #8a8a8a;
       |      	font-weight: 400;
       |      }
+      |
       |      .warning {
       |      	  background :#F1F3F2;
       |      	  border-bottom-left-radius: 6px;
@@ -28,14 +31,23 @@ object HtmlReportWriter {
       |      	  margin-bottom: 3px;
       |      	  padding: 12px;
       |      }
+      |
       |      .title {
       |      	color: #717171;
       |      	font-size: 16px;
       |      }
+      |
       |      .source {
       |        float: right;
       |      	font-style: italic;
       |      }
+      |
+      |      .snippet {
+      |        padding-top: 8px;
+      |        color: #474747;
+      |        font-weight: 300;
+      |      }
+      |
       |
     """.stripMargin
 
@@ -43,7 +55,8 @@ object HtmlReportWriter {
     <head>
       <title>Scapegoat Inspection Reporter</title>{Unparsed(
       "<link href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css\" rel=\"stylesheet\">")}{Unparsed(
-      """<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>""")}<style>
+      """<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>""")}{Unparsed
+    { """<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>"""}}<style>
       {css}
     </style>
     </head>
@@ -73,9 +86,13 @@ object HtmlReportWriter {
             case Levels.Warning => <span class="label label-warning">Warning</span>
             case Levels.Error => <span class="label label-danger">Error</span>
           }}&nbsp;{warning.text}
-          </div> <div class="snippet">
-          {warning.snippet.orNull}
-        </div>
+          </div>{warning.snippet match {
+          case None =>
+          case Some(snippet) =>
+            <div class="snippet">
+              {snippet}
+            </div>
+        }}
         </div>
     }
   }
