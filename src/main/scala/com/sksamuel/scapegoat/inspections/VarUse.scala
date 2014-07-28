bd8cb6b8618616a6af6853c338b676958996aab1
sksamuel <sam@sksamuel.com>
2014-07-28 05:44:47 +0100

    Fixed compiler warning

--- src/main/scala/com/sksamuel/scapegoat/inspections/VarUse.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/VarUse.scala
@@ -6,10 +6,12 @@ import scala.reflect.runtime._
 
 /** @author Stephen Samuel */
 class VarUse extends Inspection {
-  override def traverser(reporter: Reporter) = new universe.Traverser {
-    override def traverse(tree: scala.reflect.runtime.universe.Tree): Unit = {
+
+  import universe._
+  override def traverser(reporter: Reporter) = new Traverser {
+    override def traverse(tree: Tree): Unit = {
       tree match {
-        case universe.ValDef(modifiers, name, tpt, rhs) if modifiers.hasFlag(universe.Flag.MUTABLE) =>
+        case universe.ValDef(modifiers, name, tpt, rhs) if modifiers.hasFlag(Flag.MUTABLE) =>
           reporter.warn("Use of var", tree, level = Levels.Warning)
         case _ => super.traverse(tree)
       }
