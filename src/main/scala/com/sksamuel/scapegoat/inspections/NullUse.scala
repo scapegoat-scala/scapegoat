b4d1e06fed1a17ce48fb903dcfad9b185b6bd728
sksamuel <sam@sksamuel.com>
2014-07-28 06:56:28 +0100

    updated var to ignore synthetic

--- src/main/scala/com/sksamuel/scapegoat/inspections/NullUse.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/NullUse.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.scapegoat.inspections
 
-import com.sksamuel.scapegoat.{Levels, Inspection, Reporter}
+import com.sksamuel.scapegoat.{Inspection, Levels, Reporter}
 
 /** @author Stephen Samuel */
 class NullUse extends Inspection {
@@ -9,10 +9,15 @@ class NullUse extends Inspection {
   import scala.reflect.runtime.universe._
 
   override def traverser(reporter: Reporter) = new universe.Traverser {
+
+    def containsNull(trees: List[universe.Tree]) = trees.contains(Literal(Constant(null)))
+
     override def traverse(tree: scala.reflect.runtime.universe.Tree): Unit = {
       tree match {
-        case Literal(Constant(null)) => reporter.warn("null use", tree, Levels.Error,
-          "null used near: " + tree.toString().take(300))
+        case Apply(_, args) if containsNull(args) =>
+          reporter.warn("null use", tree, Levels.Error, "null used near: " + tree.toString().take(300))
+        case Literal(Constant(null)) =>
+          reporter.warn("null use", tree, Levels.Error, "null used: " + tree.toString().take(300))
         case _ => super.traverse(tree)
       }
     }
--- src/main/scala/com/sksamuel/scapegoat/inspections/VarUse.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/VarUse.scala
@@ -2,8 +2,6 @@ package com.sksamuel.scapegoat.inspections
 
 import com.sksamuel.scapegoat.{Inspection, Levels, Reporter}
 
-import scala.reflect.runtime._
-
 /** @author Stephen Samuel */
 class VarUse extends Inspection {
 
@@ -11,6 +9,7 @@ class VarUse extends Inspection {
   override def traverser(reporter: Reporter) = new Traverser {
     override def traverse(tree: Tree): Unit = {
       tree match {
+        case ValDef(modifiers, name, tpt, rhs) if modifiers.hasFlag(Flag.SYNTHETIC) =>
         case ValDef(modifiers, name, tpt, rhs) if modifiers.hasFlag(Flag.MUTABLE) =>
           reporter.warn("Use of var", tree, Levels.Warning, "var used: " + tree.toString().take(300))
         case _ => super.traverse(tree)
