c64efb75ba43ba0776e2e4d177cb49ea0ececf1a
sksamuel <sam@sksamuel.com>
2014-07-28 02:41:14 +0100

    Updated source file to look like package

--- src/main/scala/com/sksamuel/scapegoat/Reporter.scala
+++ src/main/scala/com/sksamuel/scapegoat/Reporter.scala
@@ -4,7 +4,10 @@ import scala.collection.mutable.ListBuffer
 
 /** @author Stephen Samuel */
 class Reporter {
+
   val warnings = new ListBuffer[Warning]
+  def warnings(level: Level): Seq[Warning] = warnings.filter(_.level == level)
+
   def warn(text: String, tree: scala.reflect.runtime.universe.Tree, level: Level): Unit = {
     warn(text, tree, level, None)
   }
@@ -12,12 +15,15 @@ class Reporter {
     warn(text, tree, level, Option(snippet))
   }
   def warn(text: String, tree: scala.reflect.runtime.universe.Tree, level: Level, snippet: Option[String]): Unit = {
-    val sourceFile = tree.pos.source.file.path
-    val indexOf = sourceFile.indexOf("src/main/scala")
-    val normalizedPath = if (indexOf == -1) sourceFile else sourceFile.drop(indexOf).drop("src/main/scala".length)
-    warnings.append(Warning(text, tree.pos.line, level, normalizedPath, snippet))
+    val sourceFile = normalizeSourceFile(tree.pos.source.file.path)
+    warnings.append(Warning(text, tree.pos.line, level, sourceFile, snippet))
   }
-  def warnings(level: Level): Seq[Warning] = warnings.filter(_.level == level)
+
+  private def normalizeSourceFile(sourceFile: String): String = {
+    val indexOf = sourceFile.indexOf("src/main/scala/")
+    val packageAndFile = if (indexOf == -1) sourceFile else sourceFile.drop(indexOf).drop("src/main/scala/".length)
+    packageAndFile.replace('/', '.').replace('\\','.')
+  }
 }
 
 case class Warning(text: String, line: Int, level: Level, sourceFile: String, snippet: Option[String])
