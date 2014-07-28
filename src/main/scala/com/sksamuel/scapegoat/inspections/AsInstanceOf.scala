43c3fac4eb307322013171324c41bca5b3b3a95f
sksamuel <sam@sksamuel.com>
2014-07-28 20:59:50 +0100

    #5 AsInstanceOf will ignore match selectors

--- src/main/scala/com/sksamuel/scapegoat/inspections/AsInstanceOf.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/AsInstanceOf.scala
@@ -14,7 +14,7 @@ class AsInstanceOf extends Inspection {
           reporter.warn("Use of asInstanceOf", tree, Levels.Warning,
             "asInstanceOf used near " + tree.toString().take(500) + ". Consider using pattern matching.")
         case DefDef(modifiers, _, _, _, _, _) if modifiers.hasFlag(Flag.SYNTHETIC) => // no further
-        case Match(selector, cases) => // ignore selector and process cases
+        case m@Match(selector, cases) => // ignore selector and process cases
           cases.foreach(traverse)
         case _ => super.traverse(tree)
       }
