512ad9b8ee5904cd064f50e4ddd328526c198280
sksamuel <sam@sksamuel.com>
2014-08-02 21:45:43 +0100

    Added collection negative index

--- src/main/scala/com/sksamuel/scapegoat/ScapegoatConfig.scala
+++ src/main/scala/com/sksamuel/scapegoat/ScapegoatConfig.scala
@@ -8,6 +8,7 @@ object ScapegoatConfig extends App {
     new AsInstanceOf,
     new BrokenOddness,
     new CatchNpe,
+    new CollectionNegativeIndex,
     new CollectionPromotionToAny,
     new ComparingFloatingPointTypes,
     new ComparingUnrelatedTypes,
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/CollectionNegativeIndexTest.scala
@@ -0,0 +1,21 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+
+/** @author Stephen Samuel */
+class CollectionNegativeIndexTest extends FreeSpec with Matchers with PluginRunner with OneInstancePerTest {
+
+  override val inspections = Seq(new CollectionNegativeIndex)
+
+  "collection negative index" - {
+    "should report warning" in {
+      val code = """object Test {
+                      List(1,2,3)(-2)
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+  }
+}
