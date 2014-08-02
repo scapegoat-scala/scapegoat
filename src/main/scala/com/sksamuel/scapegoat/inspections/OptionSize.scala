fb9998f0f8401a45534eec3a483465fbd10384ba
sksamuel <sam@sksamuel.com>
2014-08-02 19:43:17 +0100

    Added option.size test

---
+++ src/main/scala/com/sksamuel/scapegoat/inspections/OptionSize.scala
@@ -0,0 +1,23 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Feedback, Inspection, Levels}
+
+import scala.tools.nsc.Global
+
+/** @author Stephen Samuel */
+class OptionSize extends Inspection {
+
+  override def traverser(global: Global, feedback: Feedback): global.Traverser = new global.Traverser {
+
+    import global._
+
+    override def traverse(tree: Tree): Unit = {
+      tree match {
+        case Select(Apply(option2Iterable, List(opt)), TermName("size")) =>
+          if (option2Iterable.symbol.fullName == "scala.Option.option2Iterable")
+            feedback.warn("Prefer Option.isDefined instead of Option.size", tree.pos, Levels.Error, tree.toString().take(500))
+        case _ => super.traverse(tree)
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/OptionSizeTest.scala
@@ -0,0 +1,22 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers}
+
+/** @author Stephen Samuel */
+class OptionSizeTest extends FreeSpec with Matchers with PluginRunner {
+
+  override val inspections = Seq(new OptionSize)
+
+  "option.size use" - {
+    "should report warning" in {
+
+      val code = """class Test {
+                      Option("sammy").size
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+  }
+}
