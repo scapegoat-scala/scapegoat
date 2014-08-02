92490075b032824ff967818b8104561ac2fabd2a
sksamuel <sam@sksamuel.com>
2014-08-02 19:09:13 +0100

    Added exists instead of find.isDefined test

---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/UseExistsNotFindAndIsDefinedTest.scala
@@ -0,0 +1,23 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers}
+
+/** @author Stephen Samuel */
+class UseExistsNotFindAndIsDefinedTest
+  extends FreeSpec with Matchers with PluginRunner {
+
+  override val inspections = Seq(new UseExistsNotFindAndIsDefined)
+
+  "filter then size" - {
+    "should report warning" in {
+
+      val code = """object Test {
+                      val a = List(1,2,3).find(_>4).isDefined
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+  }
+}
