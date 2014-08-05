40f5bfba496b2b142fab372e29526b071f8f75c8
sksamuel <sam@sksamuel.com>
2014-08-05 09:44:36 +0100

    Added negation is empty inspection

---
+++ src/main/scala/com/sksamuel/scapegoat/inspections/collections/NegationIsEmpty.scala
@@ -0,0 +1,27 @@
+package com.sksamuel.scapegoat.inspections.collections
+
+import com.sksamuel.scapegoat.{Inspection, InspectionContext, Inspector, Levels}
+
+/** @author Stephen Samuel */
+class NegationIsEmpty extends Inspection {
+
+  def inspector(context: InspectionContext): Inspector = new Inspector(context) {
+    override def traverser = new context.Traverser {
+
+      import context.global._
+
+      private val IsEmpty = TermName("isEmpty")
+      private val Bang = TermName("unary_$bang")
+      private def isTraversable(tree: Tree) = tree.tpe <:< typeOf[Traversable[_]]
+
+      override def inspect(tree: Tree): Unit = {
+        tree match {
+          case Select(Select(lhs, IsEmpty), Bang) if isTraversable(lhs) =>
+            context.warn("!isEmpty can be replaced with nonEmpty", tree.pos, Levels.Info,
+              tree.toString().take(100), NegationIsEmpty.this)
+          case _ => continue(tree)
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/collections/NegationIsEmptyTest.scala
@@ -0,0 +1,22 @@
+package com.sksamuel.scapegoat.inspections.collections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+
+/** @author Stephen Samuel */
+class NegationIsEmptyTest extends FreeSpec with Matchers with PluginRunner with OneInstancePerTest {
+
+  override val inspections = Seq(new NegationIsEmpty)
+
+  "!isEmpty" - {
+    "should report warning" in {
+      val code = """object Test {
+                      val list = List(1,2,3)
+                      val notEmpty = !list.isEmpty
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+  }
+}
