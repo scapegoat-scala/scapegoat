9e358da4aa71120ae13d41ce28f444d582cf5573
sksamuel <sam@sksamuel.com>
2014-08-08 09:00:32 +0100

    Changed levels in output to lowercase

--- src/main/scala/com/sksamuel/scapegoat/plugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/plugin.scala
@@ -67,7 +67,7 @@ class ScapegoatComponent(val global: Global, inspections: Seq[Inspection])
 
       if (consoleOutput) {
         for ( warning <- feedback.warnings ) {
-          println(s"[${"%7s".format(warning.level)}] [scapegoat] ${warning.text} - ${warning.sourceFile}:${warning.line}")
+          println(s"[${"%7s".format(warning.level.toString.toLowerCase)}] [scapegoat] ${warning.text} - ${warning.sourceFile}:${warning.line}")
         }
       }
 
