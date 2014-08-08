11ec5cde456f78bdf5daeefce49f763ad37f6970
sksamuel <sam@sksamuel.com>
2014-08-08 21:16:56 +0100

    Fixed typos

--- src/main/scala/com/sksamuel/scapegoat/inspections/unneccesary/UnnecessaryIf.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/unneccesary/UnnecessaryIf.scala
@@ -13,17 +13,17 @@ class UnnecessaryIf extends Inspection {
       override def inspect(tree: Tree): Unit = {
         tree match {
           case If(cond, Literal(Constant(true)), Literal(Constant(false))) =>
-            context.warn("Unncessary if condition.",
+            context.warn("Unnecessary if condition.",
               tree.pos,
               Levels.Info,
-              "If comparision is not needed. Use the condition. Eg, instead of if (a ==b) true else false, simply use a == b. : " + tree
+              "If comparison is not needed. Use the condition. Eg, instead of if (a ==b) true else false, simply use a == b. : " + tree
                 .toString().take(500),
               UnnecessaryIf.this)
           case If(cond, Literal(Constant(false)), Literal(Constant(true))) =>
             context.warn("Unncessary if condition.",
               tree.pos,
               Levels.Info,
-              "If comparision is not needed. Use the negated condition. Eg, instead of if (a ==b) false else true, simply use !(a == b). : " + tree
+              "If comparison is not needed. Use the negated condition. Eg, instead of if (a ==b) false else true, simply use !(a == b). : " + tree
                 .toString().take(500),
               UnnecessaryIf.this)
           case _ => continue(tree)
