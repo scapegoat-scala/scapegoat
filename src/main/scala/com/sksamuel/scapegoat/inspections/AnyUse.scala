7169f13741803a1ca8ac42be769803b32a9f4c4d
sksamuel <sam@sksamuel.com>
2014-08-10 22:06:14 +0100

    Added any use inspection

---
+++ src/main/scala/com/sksamuel/scapegoat/inspections/AnyUse.scala
@@ -0,0 +1,37 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Inspection, InspectionContext, Inspector, Levels}
+
+import scala.reflect.internal.Flags
+
+/** @author Stephen Samuel */
+class AnyUse extends Inspection {
+
+  def inspector(context: InspectionContext): Inspector = new Inspector(context) {
+    override def traverser = new context.Traverser {
+
+      import context.global._
+
+      def warn(tree: Tree) = {
+        context.warn("AnyUse",
+          tree.pos,
+          Levels.Info,
+          "Use of Any should be avoided: " + tree.toString().take(200),
+          AnyUse.this)
+      }
+
+      override def inspect(tree: Tree): Unit = {
+        tree match {
+          case DefDef(mods, _, _, _, _, _) if mods.isSynthetic =>
+          case DefDef(mods, _, _, _, _, _) if mods.hasFlag(Flags.SetterFlags) =>
+          case DefDef(mods, _, _, _, _, _) if mods.hasFlag(Flags.GetterFlags) =>
+          case ValDef(_, _, tpt, _)
+            if tpt.tpe =:= typeOf[Any] => warn(tree)
+          case DefDef(_, _, _, _, tpt, _) if tpt.tpe =:= typeOf[Any] => warn(tree)
+          case _ => continue(tree)
+        }
+      }
+    }
+  }
+}
+
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/AnyUseTest.scala
@@ -0,0 +1,79 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+
+/** @author Stephen Samuel */
+class AnyUseTest
+  extends FreeSpec
+  with Matchers
+  with PluginRunner
+  with OneInstancePerTest {
+
+  override val inspections = Seq(new AnyUse)
+
+  "AnyUse" - {
+    "should report warning" - {
+      "for methods returning any ==" in {
+
+        val code = """class Test {
+                       def foo =  {
+                         if (1 < System.currentTimeMillis) {
+                           "sam"
+                         } else {
+                            4
+                         }
+                       }
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "for vals returning any ==" in {
+
+        val code = """class Test {
+                       val foo =  {
+                         if (1 < System.currentTimeMillis) {
+                           "sam"
+                         } else {
+                            4
+                         }
+                       }
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "for lazy vals returning any ==" in {
+
+        val code = """class Test {
+                       lazy val foo =  {
+                         if (1 < System.currentTimeMillis) {
+                           "sam"
+                         } else {
+                            4
+                         }
+                       }
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "for vars returning any ==" in {
+
+        val code = """class Test {
+                       var foo =  {
+                         if (1 < System.currentTimeMillis) {
+                           "sam"
+                         } else {
+                            4
+                         }
+                       }
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+    }
+  }
+}
