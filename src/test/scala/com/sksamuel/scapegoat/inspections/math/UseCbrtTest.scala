5e3524a9f647d3680ccca6faf8da1fa513753c51
HairyFotr <hairyfotr@gmail.com>
2014-08-16 10:17:28 +0200

    Added use cbrt inspection

---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/math/UseCbrtTest.scala
@@ -0,0 +1,26 @@
+package com.sksamuel.scapegoat.inspections.math
+
+import com.sksamuel.scapegoat.PluginRunner
+import org.scalatest.{FreeSpec, Matchers}
+
+/** @author Stephen Samuel */
+class UseCbrtTest extends FreeSpec with Matchers with PluginRunner {
+
+  override val inspections = Seq(new UseCbrt)
+
+  "broken odd use" - {
+    "should report warning" in {
+
+      val code = """object Test {
+                        val a = 2
+                        math.pow(2, 1/3d)
+                        math.pow(2, 1/3f)
+                        Math.pow(2, 1/3d)
+                        StrictMath.pow(2, 1/3d)
+                    } """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 4
+    }
+  }
+}
