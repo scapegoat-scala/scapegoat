30ee5b2e7a3c27514b684bb1de5eab06ed204c6f
sksamuel <sam@sksamuel.com>
2014-08-17 23:53:09 +0100

    Improved parameterless method test

--- src/test/scala/com/sksamuel/scapegoat/inspections/style/ParameterlessMethodReturnsUnitTest.scala
+++ src/test/scala/com/sksamuel/scapegoat/inspections/style/ParameterlessMethodReturnsUnitTest.scala
@@ -1,25 +1,44 @@
 package com.sksamuel.scapegoat.inspections.style
 
 import com.sksamuel.scapegoat.PluginRunner
-import org.scalatest.{FreeSpec, Matchers}
+import org.scalatest.{OneInstancePerTest, FreeSpec, Matchers}
 
 /** @author Stephen Samuel */
-class ParameterlessMethodReturnsUnitTest extends FreeSpec with Matchers with PluginRunner {
+class ParameterlessMethodReturnsUnitTest extends FreeSpec with Matchers with PluginRunner with OneInstancePerTest {
 
   override val inspections = Seq(new ParameterlessMethodReturnsUnit)
 
   "ParameterlessMethodReturnsUnit" - {
-    "should report warning" in {
+    "should report warning" - {
+      "for methods with unit and no params" in {
 
-      val code = """object Test {
-                   | def paramless: Unit = ()
-                   | def paramless2 : Int = 4
-                   | def params(): Unit = ()
-                   | def params2() : Int = 4
-                    } """.stripMargin
+        val code =
+          """object Test {
+               def paramless: Unit = ()
+             } """.stripMargin
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+    }
+    "should not report warning" - {
+      "for methods with unit and params" in {
 
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 1
+        val code =
+          """object Test {
+               def params(): Unit = ()
+             } """.stripMargin
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "for parameterless non unit methods" in {
+
+        val code =
+          """object Test {
+               def params: Int = 3
+             } """.stripMargin
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
     }
   }
 }
