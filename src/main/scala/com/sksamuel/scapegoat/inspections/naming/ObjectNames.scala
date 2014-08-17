7bf3af8300280fe716387867b6e184c8cfe297c9
sksamuel <sam@sksamuel.com>
2014-08-17 22:31:02 +0100

    Updated object names inspection

--- src/main/scala/com/sksamuel/scapegoat/inspections/naming/ObjectNames.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/naming/ObjectNames.scala
@@ -10,7 +10,7 @@ class ObjectNames extends Inspection {
 
       import context.global._
 
-      private val regex = "^[A-Z][A-Za-z]*$"
+      private val regex = "^[A-Za-z]*$"
 
       override def inspect(tree: Tree): Unit = {
         tree match {
