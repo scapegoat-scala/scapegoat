889c4bcae71415e267ac1ecfe8e7f0758df8e1bf
Paul Phillips <paulp@improving.org>
2014-08-21 14:46:51 -0700

    Improve the contains test.

--- src/test/scala/com/sksamuel/scapegoat/inspections/collections/UnsafeContainsTest.scala
+++ src/test/scala/com/sksamuel/scapegoat/inspections/collections/UnsafeContainsTest.scala
@@ -10,18 +10,26 @@ class UnsafeContainsTest extends FreeSpec with Matchers with PluginRunner {
 
   "unsafe contains" - {
     "should report warning" in {
-      val code = """object Test {
-                      List(1).contains("sam")
-                      val int = 1
-                      List("sam").contains(int)
-                      List(2).contains(int) // is good
-                      List(new RuntimeException, new Exception).contains(new RuntimeException) // good
-                      val name = "RuntimeException"
-                      List(new RuntimeException).contains(name) //bad
-                    } """.stripMargin
+      val code = """
+      |object Test {
+      |  import scala.language.higherKinds
+      |  def f1[A](xs: Seq[A], y: A)                                        = xs contains y  // good
+      |  def f2[A <: AnyRef](xs: Seq[A], y: Int)                            = xs contains y  // bad
+      |  def f3[A <: AnyRef](xs: Vector[A], y: Int)                         = xs contains y  // bad
+      |  def f4[CC[X] <: Seq[X], A](xs: CC[A], y: A)                        = xs contains y  // good
+      |  def f5[CC[X] <: Seq[X], A <: AnyRef, B <: AnyVal](xs: CC[A], y: B) = xs contains y  // bad
+      |
+      |  List(1).contains("sam")
+      |  val int = 1
+      |  List("sam").contains(int)
+      |  List(2).contains(int) // is good
+      |  List(new RuntimeException, new Exception).contains(new RuntimeException) // good
+      |  val name = "RuntimeException"
+      |  List(new RuntimeException).contains(name) // bad
+      |}""".stripMargin.trim
 
       compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 3
+      compiler.scapegoat.feedback.warnings.size shouldBe 6
     }
   }
 }
