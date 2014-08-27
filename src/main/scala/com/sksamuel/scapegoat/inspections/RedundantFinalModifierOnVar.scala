11dc689dce20e6d4b134aa8eac91dfce16eaa4de
sksamuel <sam@sksamuel.com>
2014-08-27 20:30:46 +0100

    Added RedundantFinalModifierOnVar inspection

---
+++ src/main/scala/com/sksamuel/scapegoat/inspections/RedundantFinalModifierOnVar.scala
@@ -0,0 +1,31 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Inspection, InspectionContext, Inspector, Levels}
+
+class RedundantFinalModifierOnVar extends Inspection {
+
+  override def inspector(context: InspectionContext): Inspector = new Inspector(context) {
+
+    import context.global._
+
+    override def postTyperTraverser = Some apply new context.Traverser {
+
+      override def inspect(tree: Tree): Unit = {
+        tree match {
+          case ValDef(mods, _, _, _)
+            if mods.isFinal && mods.isMutable &&
+              (tree.symbol.enclClass.isFinal ||
+                tree.symbol.enclClass.isCase ||
+                tree.symbol.enclClass.isModuleOrModuleClass ||
+                tree.symbol.enclClass.isPackageObjectOrClass) =>
+            context.warn("Redundant final modifier on method",
+              tree.pos,
+              Levels.Info,
+              "Method cannot be overriden, final modifer is redundant",
+              RedundantFinalModifierOnVar.this)
+          case _ => continue(tree)
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/sksamuel/scapegoat/inspections/RedundantFinalModifierOnVarTest.scala
@@ -0,0 +1,77 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.PluginRunner
+
+import org.scalatest.{FreeSpec, Matchers, OneInstancePerTest}
+
+/** @author Stephen Samuel */
+class RedundantFinalModifierOnVarTest
+  extends FreeSpec
+  with Matchers
+  with PluginRunner
+  with OneInstancePerTest {
+
+  override val inspections = Seq(new RedundantFinalModifierOnVar)
+
+  "RedundantFinalModifierOnVar" - {
+    "should report warning" - {
+      "when object has final var" in {
+        val code = """object Test {  final var foo = {}  } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "when final class has final var" in {
+        val code = """final class Test {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "when case class has final var" in {
+        val code = """case class Test() {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "when case object has final var" in {
+        val code = """case object Test {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "when implicit object has final var" in {
+        val code = """object A { implicit object B {  final var foo = {} } } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+    }
+    "should not report warning" - {
+      "when non final class has final var" in {
+        val code = """class Test {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "when trait has final var" in {
+        val code = """trait Test {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "when abstract class has final var" in {
+        val code = """abstract class Test {  final var foo = {} } """
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "when implicit class has final var" in {
+        val code = """object A { implicit class B(str:String) {  final var foo = {} } }"""
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "on val fields" in {
+        val code = """object A { val b = true }"""
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "on non final var fields" in {
+        val code = """object A { var b = true }"""
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+    }
+  }
+}
