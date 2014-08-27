9fb32c71440e9b15164d0bbb2e4e419d6492bc9a
sksamuel <sam@sksamuel.com>
2014-08-28 00:34:37 +0100

    Fixed duplicated import

--- src/main/scala/com/sksamuel/scapegoat/inspections/imports/DuplicateImport.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/imports/DuplicateImport.scala
@@ -17,9 +17,9 @@ class DuplicateImport extends Inspection {
 
       override def inspect(tree: Tree): Unit = {
         tree match {
-          case PackageDef(_, _)     => imports.clear()
-          case ModuleDef(_, _, _)   => imports.clear()
-          case ClassDef(_, _, _, _) => imports.clear()
+          case PackageDef(_, _) => imports.clear(); continue(tree)
+          case ModuleDef(_, _, _) => imports.clear(); continue(tree)
+          case ClassDef(_, _, _, _) => imports.clear(); continue(tree)
           case Import(expr, selectors) =>
             selectors.foreach(selector => {
               val name = expr.toString + "." + selector.name
@@ -29,7 +29,7 @@ class DuplicateImport extends Inspection {
               imports.add(name)
             })
           case DefDef(_, _, _, _, _, _) => // check imports inside defs
-          case _                        => continue(tree)
+          case _ => continue(tree)
         }
       }
     }
