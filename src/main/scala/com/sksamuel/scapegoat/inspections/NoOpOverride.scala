0b44ff7b3d452b7947f8c66c4555ca31c2cf23b8
Grzegorz Oledzki <goledzki@sumologic.com>
2019-11-05 22:26:19 +0100

    Checking argument names from signature to actual super call

--- src/main/scala/com/sksamuel/scapegoat/inspections/NoOpOverride.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/NoOpOverride.scala
@@ -10,11 +10,20 @@ class NoOpOverride extends Inspection("No op Override", Levels.Info) {
 
       import context.global._
 
+      private def argumentsMatch(signatureArgs: List[ValDef], actualArgs: List[Tree]) = {
+        signatureArgs.size == actualArgs.size &&
+         signatureArgs.zip(actualArgs).forall {
+          case (sig, act: Ident) => sig.name == act.name
+          case _ => false
+         }
+      }
+
       override def inspect(tree: Tree): Unit = {
         tree match {
-          case DefDef(mods, name, _, vparamss, _, Apply(Select(Super(This(_), _), name2), args)) if name == name2 && vparamss.foldLeft(0)((a, b) => a + b.size) == args.size =>
-            context.warn(tree.pos, self,
-              "This method is overridden yet only calls super: " + tree.toString().take(200))
+          case DefDef(mods, name, _, vparamss, _, Apply(Select(Super(This(_), _), name2), args))
+            if name == name2 && vparamss.size == 1 && argumentsMatch(vparamss.head, args) =>
+              context.warn(tree.pos, self,
+                "This method is overridden yet only calls super: " + tree.toString().take(200))
           case _ => continue(tree)
         }
       }
