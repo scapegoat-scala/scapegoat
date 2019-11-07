596ed74d32d9a7da51f5669acd1958dcd5852d91
Grzegorz Oledzki <goledzki@sumologic.com>
2019-11-07 22:06:29 +0100

    Specific collection types instead of Traversable

--- src/main/scala/com/sksamuel/scapegoat/inspections/collections/ExistsSimplifiableToContains.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/collections/ExistsSimplifiableToContains.scala
@@ -16,16 +16,22 @@ class ExistsSimplifiableToContains extends Inspection("Exists simplifiable to co
 
       private val Equals = TermName("$eq$eq")
 
-      private def isContainsType(container: Tree, value: Tree): Boolean = {
+      private def doesElementTypeMatch(container: Tree, value: Tree): Boolean = {
         val valueType = value.tpe.underlying.typeSymbol.tpe
         val traversableType = container.tpe.underlying.baseType(typeOf[Traversable[Any]].typeSymbol)
         traversableType.typeArgs.exists(t => valueType <:< t || valueType =:= t)
       }
 
+      private def isContainsTraversable(tree: Tree): Boolean = {
+        // Traversable itself doesn't include a .contains() method
+        isSet(tree) || isSeq(tree) || isList(tree) || isMap(tree)
+      }
+
       override def inspect(tree: Tree): Unit = {
         tree match {
-          case Apply(Select(lhs, TermName("exists")), List(Function(_, Apply(Select(_, Equals), List(x))))) if isTraversable(lhs) && isContainsType(lhs, x) =>
-            context.warn(tree.pos, self, "exists(x => x == y) can be replaced with contains(y): " + tree.toString().take(500))
+          case Apply(Select(lhs, TermName("exists")), List(Function(_, Apply(Select(_, Equals), List(x)))))
+            if isContainsTraversable(lhs) && doesElementTypeMatch(lhs, x) =>
+              context.warn(tree.pos, self, "exists(x => x == y) can be replaced with contains(y): " + tree.toString().take(500))
           case _ => continue(tree)
         }
       }
