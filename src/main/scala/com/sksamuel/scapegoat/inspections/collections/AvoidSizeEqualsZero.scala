509131c388c7a9bf29b560daa9d60542d6953aaf
Greg Oledzki <mccartney@users.noreply.github.com>
2020-03-12 14:25:18 +0100

    Update src/main/scala/com/sksamuel/scapegoat/inspections/collections/AvoidSizeEqualsZero.scala

--- src/main/scala/com/sksamuel/scapegoat/inspections/collections/AvoidSizeEqualsZero.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/collections/AvoidSizeEqualsZero.scala
@@ -7,7 +7,7 @@ class AvoidSizeEqualsZero extends Inspection(
   text = "Avoid Traversable.size == 0",
   defaultLevel = Levels.Warning,
   description = "Checks for use of Traversable.size.",
-  explanation = "Traversable.size can be slow for some data structure, prefer Traversable.isEmpty, which is O(1)."
+  explanation = "Traversable.size can be slow for some data structures, prefer Traversable.isEmpty, which is O(1)."
 ) {
 
   def inspector(context: InspectionContext): Inspector = new Inspector(context) {
