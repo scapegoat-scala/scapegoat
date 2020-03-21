99b008be34e91210cc76078dcc68a1048756efd4
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2020-03-21 20:59:50 +0100

    Refactoring of test structure (should, should not - as top level entries)

--- src/test/scala/com/sksamuel/scapegoat/inspections/collections/CollectionPromotionToAnyTest.scala
+++ src/test/scala/com/sksamuel/scapegoat/inspections/collections/CollectionPromotionToAnyTest.scala
@@ -14,94 +14,80 @@ class CollectionPromotionToAnyTest
 
   override val inspections = Seq(new CollectionPromotionToAny)
 
-  "lists using colon add with list" - {
-    "should report warning" in {
-      val code = """object Test {
-                   |  val a = List(1, 2, 3)
-                   |  val b = List(4, 5, 6)
-                   |  val c = a :+ b
+  "CollectionPromotionToAny" - {
+    "should report warning" - {
+      "lists using colon add with list" in {
+        val code = """object Test {
+                     |  val a = List(1, 2, 3)
+                     |  val b = List(4, 5, 6)
+                     |  val c = a :+ b
                     } """.stripMargin
 
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 1
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "seqs using colon add with list" in {
+        val code = """object Test {
+                     |  val a = Seq(1, 2, 3)
+                     |  val b = List(4, 5, 6)
+                     |  val c = a :+ b
+                  } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "lists using colon add with seq" in {
+        val code = """object Test {
+                     |  val a = List(1, 2, 3)
+                     |  val b = Seq(4, 5, 6)
+                     |  val c = a :+ b
+                  } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
+      "seqs using colon add with seq" in {
+        val code = """object Test {
+                     |  val a = Seq(1, 2, 3)
+                     |  val b = Seq(4, 5, 6)
+                     |  val c = a :+ b
+                  } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 1
+      }
     }
-  }
+    "should not report warning" - {
+      "Vectors using colon add with non seq" in {
+        val code = """object Test {
+                     |        val a = Vector(1, 2, 3)
+                     |        val b = 6
+                     |        val c = a :+ b
+                  } """.stripMargin
 
-  "seqs using colon add with list" - {
-    "should report warning" in {
-      val code = """object Test {
-                   |  val a = Seq(1, 2, 3)
-                   |  val b = List(4, 5, 6)
-                   |  val c = a :+ b
-                    } """.stripMargin
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "lists using colon add with non seq" in {
+        val code = """object Test {
+                     |        val a = List(1, 2, 3)
+                     |        val b = 4
+                     |        val c = a :+ b
+                  } """.stripMargin
 
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 1
-    }
-  }
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "list[any] using colon add" in {
+        val code = """object Test {
+                     |        val a = List[Any](1, 2, 3)
+                     |        val b = "string"
+                     |        val c = a :+ b
+                  } """.stripMargin
 
-  "lists using colon add with seq" - {
-    "should report warning" in {
-      val code = """object Test {
-                   |  val a = List(1, 2, 3)
-                   |  val b = Seq(4, 5, 6)
-                   |  val c = a :+ b
-                    } """.stripMargin
-
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 1
-    }
-  }
-
-  "seqs using colon add with seq" - {
-    "should report warning" in {
-      val code = """object Test {
-                   |  val a = Seq(1, 2, 3)
-                   |  val b = Seq(4, 5, 6)
-                   |  val c = a :+ b
-                    } """.stripMargin
-
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 1
-    }
-  }
-
-  "Vectors using colon add with non seq" - {
-    "should not report warning" in {
-      val code = """object Test {
-                   |        val a = Vector(1, 2, 3)
-                   |        val b = 6
-                   |        val c = a :+ b
-                    } """.stripMargin
-
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 0
-    }
-  }
-
-  "lists using colon add with non seq" - {
-    "should not report warning" in {
-      val code = """object Test {
-                   |        val a = List(1, 2, 3)
-                   |        val b = 4
-                   |        val c = a :+ b
-                    } """.stripMargin
-
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 0
-    }
-  }
-
-  "list[any] using colon add" - {
-    "should not report warning" in {
-      val code = """object Test {
-                   |        val a = List[Any](1, 2, 3)
-                   |        val b = "string"
-                   |        val c = a :+ b
-                    } """.stripMargin
-
-      compileCodeSnippet(code)
-      compiler.scapegoat.feedback.warnings.size shouldBe 0
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
     }
   }
 }
