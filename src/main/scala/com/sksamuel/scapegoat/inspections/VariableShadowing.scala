7fe4cf252adb44e1d3c806c8c8e9f55c3ae05651
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2020-05-07 21:52:50 +0200

    Function opens a new context too

--- src/main/scala/com/sksamuel/scapegoat/inspections/VariableShadowing.scala
+++ src/main/scala/com/sksamuel/scapegoat/inspections/VariableShadowing.scala
@@ -14,48 +14,46 @@ class VariableShadowing
         "Variable shadowing is very useful, but can easily lead to nasty bugs in your code. Shadowed variables can be potentially confusing to other maintainers when the same name is adopted to have a new meaning in a nested scope."
     ) {
 
-  def inspector(context: InspectionContext): Inspector = new Inspector(context) {
-    override def postTyperTraverser = new context.Traverser {
+  def inspector(context: InspectionContext): Inspector =
+    new Inspector(context) {
+      override def postTyperTraverser =
+        new context.Traverser {
 
-      import context.global._
+          import context.global._
 
-      private val contexts = new mutable.Stack[ListBuffer[String]]()
+          private val contexts = new mutable.Stack[ListBuffer[String]]()
 
-      private def isDefined(name: String): Boolean = contexts exists (_.contains(name.trim))
+          private def isDefined(name: String): Boolean = contexts exists (_.contains(name.trim))
 
-      private def enter(): Unit = contexts.push(new ListBuffer[String])
-      private def exit(): Unit = contexts.pop()
+          private def enter(): Unit = contexts.push(new ListBuffer[String])
+          private def exit(): Unit = contexts.pop()
 
-      override def inspect(tree: Tree): Unit = {
-        tree match {
-          case Block(_, _) =>
-            enter(); continue(tree); exit()
-          case ClassDef(_, _, _, Template(_, _, _)) =>
-            enter(); continue(tree); exit()
-          case ModuleDef(_, _, Template(_, _, _)) =>
-            enter(); continue(tree); exit()
-          case DefDef(_, name, _, vparamss, _, rhs) =>
-            enter()
-            // For case classes there's a synthetic constructor (not marked as <synthentic>) which takes
-            // the same argument name.
-            if (name.toString != "<init>") {
-              vparamss.foreach(_.foreach(inspect))
+          override def inspect(tree: Tree): Unit = {
+            tree match {
+              case Block(_, _) | ClassDef(_, _, _, Template(_, _, _)) | ModuleDef(_, _, Template(_, _, _)) |
+                  Function(_, _) =>
+                enter(); continue(tree); exit()
+              case DefDef(_, name, _, vparamss, _, rhs) =>
+                enter()
+                // For case classes there's a synthetic constructor (not marked as <synthentic>) which takes
+                // the same argument name.
+                if (name.toString != "<init>")
+                  vparamss.foreach(_.foreach(inspect))
+                inspect(rhs)
+                exit()
+              case ValDef(_, TermName(name), _, _) =>
+                if (isDefined(name)) context.warn(tree.pos, self, tree.toString.take(200))
+                contexts.top.append(name.trim)
+              case Match(_, cases) =>
+                cases.foreach {
+                  case CaseDef(Bind(name, _), _, _) =>
+                    if (isDefined(name.toString)) context.warn(tree.pos, self, tree.toString.take(200))
+                  case _ => // do nothing
+                }
+                continue(tree)
+              case _ => continue(tree)
             }
-            inspect(rhs)
-            exit()
-          case ValDef(_, TermName(name), _, _) =>
-            if (isDefined(name)) context.warn(tree.pos, self, tree.toString.take(200))
-            contexts.top.append(name.trim)
-          case Match(_, cases) =>
-            cases.foreach {
-              case CaseDef(Bind(name, _), _, _) =>
-                if (isDefined(name.toString)) context.warn(tree.pos, self, tree.toString.take(200))
-              case _ => // do nothing
-            }
-            continue(tree)
-          case _ => continue(tree)
+          }
         }
-      }
     }
-  }
 }
