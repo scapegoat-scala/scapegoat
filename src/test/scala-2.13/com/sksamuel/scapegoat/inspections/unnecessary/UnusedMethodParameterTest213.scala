4a61bc36509c02da9d9ebc551c68e3a48611197f
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2020-10-14 22:18:20 +0200

    Scala 2.13-only for @unused

---
+++ src/test/scala-2.13/com/sksamuel/scapegoat/inspections/unnecessary/UnusedMethodParameterTest213.scala
@@ -0,0 +1,27 @@
+package com.sksamuel.scapegoat.inspections.unnecessary
+
+import com.sksamuel.scapegoat.InspectionTest
+import com.sksamuel.scapegoat.inspections.unneccesary.UnusedMethodParameter
+
+class UnusedMethodParameterTest213 extends InspectionTest {
+
+  override val inspections = Seq(new UnusedMethodParameter)
+
+  "UnusedMethodParameter in Scala 2.13" - {
+    "should not report warning" - {
+      "for unused parameters if they are annotated with @unused (#340)" in {
+        val code =
+          """| import scala.annotation.unused
+             |  class Test {
+             |   def foo(@unused a:String, b: Int): Unit = {
+             |     println(b)
+             |   }
+             | } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+        compiler.scapegoat.feedback.warns.size shouldBe 0
+      }
+    }
+  }
+}
