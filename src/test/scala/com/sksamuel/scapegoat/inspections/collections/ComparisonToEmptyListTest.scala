8610374d72b88605a0e2f229c01e16a35bc54d54
Damian Bronecki <bronecki.damian@gmail.com>
2021-01-02 21:49:06 +0100

    Fix false positive in ComparisonToEmptyList inspection (#461)
    In my previous commit 9ccd7405 there was no check verifying that 'apply' method is called on 'List' type, and because of that false positives were reported in cases like this:
    
    ```
    object Test {
        case class Paramless[T]()
    
        val a = List(1,2,3)
        val b = a == Paramless()
    }
    ```

--- src/test/scala/com/sksamuel/scapegoat/inspections/collections/ComparisonToEmptyListTest.scala
+++ src/test/scala/com/sksamuel/scapegoat/inspections/collections/ComparisonToEmptyListTest.scala
@@ -70,5 +70,29 @@ class ComparisonToEmptyListTest extends InspectionTest {
         compiler.scapegoat.feedback.warnings.size shouldBe 1
       }
     }
+    "should not report warning" - {
+      "for custom parameterless case class with type parameter on rhs" in {
+        val code = """object Test {
+                        case class Paramless[T]()
+
+                        val a = List(1,2,3)
+                        val b = a == Paramless()
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+      "for custom parameterless case class with type parameter on lhs" in {
+        val code = """object Test {
+                        case class Paramless[T]()
+
+                        val a = List(1,2,3)
+                        val b = Paramless() == a
+                    } """.stripMargin
+
+        compileCodeSnippet(code)
+        compiler.scapegoat.feedback.warnings.size shouldBe 0
+      }
+    }
   }
 }
