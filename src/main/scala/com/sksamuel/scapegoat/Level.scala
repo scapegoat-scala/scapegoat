22642edf37e5a3a19ef7aba429c408265c03aa18
Bendix Saeltz <bendix@saeltz.de>
2022-01-13 14:52:27 +0100

    Add ScalaFmt dialect

--- src/main/scala/com/sksamuel/scapegoat/Level.scala
+++ src/main/scala/com/sksamuel/scapegoat/Level.scala
@@ -1,7 +1,8 @@
 package com.sksamuel.scapegoat
 
 /**
- * @author Stephen Samuel
+ * @author
+ *   Stephen Samuel
  */
 sealed trait Level
 
@@ -15,12 +16,12 @@ object Levels {
   case object Error extends Level
 
   /**
-   * Warnings are reserved for code that has bad semantics.
-   * This by itself does not necessarily mean the code is buggy, but could mean the developer
-   * made a mistake or does not fully understand the contructs or best practice.
+   * Warnings are reserved for code that has bad semantics. This by itself does not necessarily mean the code
+   * is buggy, but could mean the developer made a mistake or does not fully understand the contructs or best
+   * practice.
    *
-   * An example is an expression as a statement. While this is perfectly legal, it could indicate
-   * that the developer meant to assign the result to or otherwise use it.
+   * An example is an expression as a statement. While this is perfectly legal, it could indicate that the
+   * developer meant to assign the result to or otherwise use it.
    *
    * Another example is a constant if. You can do things like if (true) { } if you want, but since the block
    * will always evaluate, the if statement perhaps indicates a mistake.
@@ -30,12 +31,13 @@ object Levels {
   /**
    * Infos are used for code which is semantically fine, but there exists a more idomatic way of writing it.
    *
-   * An example would be using an if statement to return true or false as the last statement in a block.
-   * Eg,
+   * An example would be using an if statement to return true or false as the last statement in a block. Eg,
    *
+   * {{{
    * def foo = {
    *   if (a) true else false
    * }
+   * }}}
    *
    * Can be re-written as
    *
