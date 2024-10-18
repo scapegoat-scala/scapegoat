e6fd22a91ecdb8b866dec846ca37f6d4135835f8
mike <mike.fch1@gmail.com>
2024-10-18 22:05:40 +0300

    require check for scala 2

---
+++ src/main/scala-2/com/sksamuel/scapegoat/inspections/AvoidRequire.scala
@@ -0,0 +1,31 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Inspection, InspectionContext, Inspector, Levels}
+
+class AvoidRequire extends
+  Inspection(
+    text = "Use of require",
+    defaultLevel = Levels.Warning,
+    description = "Use require in code",
+    explanation = "Using require throws an untyped Exception."
+  ) {
+
+  def inspector(ctx: InspectionContext): Inspector =
+    new Inspector(ctx) {
+      override def postTyperTraverser: context.Traverser =
+        new context.Traverser {
+
+          import context.global._
+
+          override def inspect(tree: Tree): Unit = {
+            tree match {
+              case Select(lhs, TermName("require")) if lhs.tpe.typeSymbol.fullName == "scala.Predef" =>
+                context.warn(tree.pos, self, tree.toString.take(200))
+              case _ =>
+                continue(tree)
+            }
+          }
+        }
+    }
+
+}
---
+++ src/test/scala-2/com/sksamuel/scapegoat/inspections/AvoidRequireTest.scala
@@ -0,0 +1,46 @@
+package com.sksamuel.scapegoat.inspections
+
+import com.sksamuel.scapegoat.{Inspection, InspectionTest}
+
+class AvoidRequireTest extends InspectionTest {
+  override val inspections = Seq[Inspection](new AvoidRequire)
+
+  "require use" - {
+    "should return warning in method" in {
+      val code =
+        """
+          object Test {
+            def test(x: Int): Int = {
+              require(x == 1)
+              x
+            }
+          }
+          """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+
+    "should return warning in class" in {
+      val code =
+        """
+          class Test(val x: Int) {
+            require(x == 1, "oops")
+          }
+          """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 1
+    }
+
+    "should not return warning if no require" in {
+      val code =
+        """
+          class Test(val x: Int) { }
+          """.stripMargin
+
+      compileCodeSnippet(code)
+      compiler.scapegoat.feedback.warnings.size shouldBe 0
+    }
+  }
+}
