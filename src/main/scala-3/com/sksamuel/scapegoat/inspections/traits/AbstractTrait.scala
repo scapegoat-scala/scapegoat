2a3bc47bbbc92c74d71f7ab117a4a85c8b21bc3c
mike <mike.fch1@gmail.com>
2024-10-18 21:45:05 +0300

    add abstract trait to scala3

---
+++ src/main/scala-3/com/sksamuel/scapegoat/inspections/traits/AbstractTrait.scala
@@ -0,0 +1,41 @@
+package com.sksamuel.scapegoat.inspections.traits
+
+import com.sksamuel.scapegoat.*
+import dotty.tools.dotc.ast.Trees.*
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Flags
+import dotty.tools.dotc.core.StdNames.*
+import dotty.tools.dotc.core.Types.TypeRef
+import dotty.tools.dotc.core.Symbols.ClassSymbol
+import dotty.tools.dotc.util.SourcePosition
+
+class AbstractTrait
+  extends Inspection(
+    text = "Use of abstract trait",
+    defaultLevel = Levels.Info,
+    description = "Traits are automatically abstract.",
+    explanation = "The abstract modifier is used in class definitions. It is redundant for traits, and mandatory for all other classes which have incomplete members."
+  ) {
+
+  import tpd.*
+
+  def inspect(feedback: Feedback[SourcePosition], tree: tpd.Tree)(using ctx: Context): Unit = {
+    val traverser = new InspectionTraverser {
+      def traverse(tree: Tree)(using Context): Unit = {
+        tree match {
+          case tDef: TypeDef =>
+            tDef.tpe match {
+              case TypeRef(_, kls: ClassSymbol) if kls.flags.is(Flags.Trait) && kls.flags.is(Flags.Abstract) =>
+                feedback.warn(tree.sourcePos, self, tree.asSnippet)
+              case _ =>
+            }
+
+          case _ =>
+            traverseChildren(tree)
+        }
+      }
+    }
+    traverser.traverse(tree)
+  }
+}
