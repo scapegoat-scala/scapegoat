ffe90c589d0000eaad161d0f6ac8a4016e5c4dce
mike <mike.fch1@gmail.com>
2024-10-20 21:57:12 +0300

    Add abstract trait inspections to scala3 (#889)
    * add abstract trait to scala3
    
    * remove unused imports
    
    * reorder imports for scalafix

--- src/main/scala-3/com/sksamuel/scapegoat/Inspections.scala
+++ src/main/scala-3/com/sksamuel/scapegoat/Inspections.scala
@@ -1,11 +1,13 @@
 package com.sksamuel.scapegoat
 
 import com.sksamuel.scapegoat.inspections.option._
+import com.sksamuel.scapegoat.inspections.traits._
 
 object Inspections {
 
   final val inspections: List[Inspection] = List(
-    new OptionGet
+    new OptionGet,
+    new AbstractTrait
   )
 
 }
