52c7d3de75c2616c32048bc7760437128798214e
Greg Oledzki <762437+mccartney@users.noreply.github.com>
2025-11-28 09:37:27 +0100

    Porting ComparingFloatingPointTypes inspection to Scala 3 (#1003)
    * Porting ComparingFloatingPointTypes inspection to Scala 3
    
    * sbt scalafmtAll
    
    * 15.6d

---
+++ src/main/scala-3/com/sksamuel/scapegoat/inspections/equality/ComparingFloatingPointTypes.scala
@@ -0,0 +1,37 @@
+package com.sksamuel.scapegoat.inspections.equality
+
+import com.sksamuel.scapegoat.*
+import dotty.tools.dotc.ast.Trees.*
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.util.SourcePosition
+
+class ComparingFloatingPointTypes
+    extends Inspection(
+      text = "Floating type comparison",
+      defaultLevel = Levels.Error,
+      description = "Checks for equality checks on floating point types.",
+      explanation =
+        "Due to minor rounding errors, it is not advisable to compare floating-point numbers using the == operator. Either use a threshold based comparison, or switch to a BigDecimal."
+    ) {
+
+  import tpd.*
+
+  def inspect(feedback: Feedback[SourcePosition], tree: tpd.Tree)(using ctx: Context): Unit = {
+    val traverser = new InspectionTraverser {
+      def traverse(tree: Tree)(using Context): Unit = {
+        tree match {
+          case Apply(Select(left, name), List(right)) if name.toString == "==" || name.toString == "!=" =>
+            val leftTypeName = left.tpe.typeSymbol.fullName.toString
+            val rightTypeName = right.tpe.typeSymbol.fullName.toString
+            val leftFloating = leftTypeName == "scala.Double" || leftTypeName == "scala.Float"
+            val rightFloating = rightTypeName == "scala.Double" || rightTypeName == "scala.Float"
+            if (leftFloating && rightFloating)
+              feedback.warn(tree.sourcePos, self, tree.asSnippet)
+          case _ => traverseChildren(tree)
+        }
+      }
+    }
+    traverser.traverse(tree)
+  }
+}
---
+++ src/test/scala-3/com/sksamuel/scapegoat/inspections/equality/ComparingFloatingPointTypesTest.scala
@@ -0,0 +1,82 @@
+package com.sksamuel.scapegoat.inspections.equality
+
+import com.sksamuel.scapegoat.{InspectionTest, Levels}
+
+class ComparingFloatingPointTypesTest extends InspectionTest(classOf[ComparingFloatingPointTypes]) {
+
+  "comparing floating point types" - {
+    "should report warning for double comparison with ==" in {
+      val code = """class Test {
+                      def compareFloats: Boolean = {
+                        val a = 14.5
+                        val b = 15.6
+                        a == b
+                      }
+                    }""".stripMargin
+
+      val feedback = runner.compileCodeSnippet(code)
+      feedback.errors.assertable shouldEqual Seq(
+        warning(4, Levels.Error, Some("a == b"))
+      ).assertable
+    }
+
+    "should report warning for double comparison with !=" in {
+      val code = """class Test {
+                      def compareFloats: Boolean = {
+                        val a = 14.5
+                        val b = 15.6
+                        a != b
+                      }
+                    }""".stripMargin
+
+      val feedback = runner.compileCodeSnippet(code)
+      feedback.errors.assertable shouldEqual Seq(
+        warning(4, Levels.Error, Some("a != b"))
+      ).assertable
+    }
+
+    "should report warning for float comparison with ==" in {
+      val code = """class Test {
+                      def compareFloats: Boolean = {
+                        val a = 14.5f
+                        val b = 15.6f
+                        a == b
+                      }
+                    }""".stripMargin
+
+      val feedback = runner.compileCodeSnippet(code)
+      feedback.errors.assertable shouldEqual Seq(
+        warning(4, Levels.Error, Some("a == b"))
+      ).assertable
+    }
+
+    "should report warning for float comparison with !=" in {
+      val code = """class Test {
+                      def compareFloats: Boolean = {
+                        val a = 14.5f
+                        val b = 15.6d
+                        a != b
+                      }
+                    }""".stripMargin
+
+      val feedback = runner.compileCodeSnippet(code)
+      feedback.errors.assertable shouldEqual Seq(
+        warning(4, Levels.Error, Some("a != b"))
+      ).assertable
+    }
+
+    "should not report warning for integer comparison" in {
+      val code = """class Test {
+                      def compareInts: Boolean = {
+                        val a = 14
+                        val b = 15
+                        a == b
+                      }
+                    }""".stripMargin
+
+      val feedback = runner.compileCodeSnippet(code)
+      feedback.errors.assertable shouldEqual Seq().assertable
+    }
+  }
+
+}
