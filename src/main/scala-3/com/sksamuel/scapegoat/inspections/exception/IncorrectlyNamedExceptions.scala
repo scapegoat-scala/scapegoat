605e51a5eb53513c788feaab4f5932015416a6d4
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2025-11-28 19:28:57 +0100

    8 exception inspections ported to Scala 3

---
+++ src/main/scala-3/com/sksamuel/scapegoat/inspections/exception/IncorrectlyNamedExceptions.scala
@@ -0,0 +1,57 @@
+package com.sksamuel.scapegoat.inspections.exception
+
+import com.sksamuel.scapegoat.*
+import dotty.tools.dotc.ast.Trees.*
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.util.SourcePosition
+
+class IncorrectlyNamedExceptions
+    extends Inspection(
+      text = "Incorrectly named exceptions",
+      defaultLevel = Levels.Error,
+      description = "Checks for exceptions that are not called *Exception and vice versa.",
+      explanation =
+        "Class named exception does not derive from Exception / class derived from Exception is not named *Exception."
+    ) {
+
+  import tpd.*
+
+  def inspect(feedback: Feedback[SourcePosition], tree: tpd.Tree)(using ctx: Context): Unit = {
+    val traverser = new InspectionTraverser {
+      def traverse(tree: Tree)(using Context): Unit = {
+        tree match {
+          case cdef @ TypeDef(name, impl) if cdef.isClassDef =>
+            val isNamedException = name.toString.endsWith("Exception")
+            val isAnon = scala.util.Try {
+              cdef.symbol.isAnonymousClass
+            }.getOrElse(false)
+
+            val extendsException = impl.tpe <:< ctx.definitions.ExceptionClass.typeRef
+            val selfTypeIsException = impl match {
+              case Template(_, _, self, _) =>
+                self.tpt.tpe <:< ctx.definitions.ExceptionClass.typeRef
+              case _ => false
+            }
+
+            // A class or trait is an Exception for our purposes if it either
+            // inherits from exception or it is a trait which declares its
+            // self-type to be Exception
+            val isException = extendsException || selfTypeIsException
+
+            (isNamedException, isAnon, isException) match {
+              case (true, _, false) =>
+                feedback.warn(tree.sourcePos, self, tree.asSnippet.map(_.take(500)))
+              case (false, false, true) =>
+                feedback.warn(tree.sourcePos, self, tree.asSnippet.map(_.take(500)))
+              case _ =>
+            }
+            traverseChildren(tree)
+          case _ =>
+            traverseChildren(tree)
+        }
+      }
+    }
+    traverser.traverse(tree)
+  }
+}
---
+++ src/main/scala-3/com/sksamuel/scapegoat/inspections/exception/SwallowedException.scala
@@ -0,0 +1,79 @@
+package com.sksamuel.scapegoat.inspections.exception
+
+import com.sksamuel.scapegoat.*
+import dotty.tools.dotc.ast.Trees.*
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Names.Name
+import dotty.tools.dotc.util.SourcePosition
+
+@SuppressWarnings(Array("IncorrectlyNamedExceptions"))
+class SwallowedException
+    extends Inspection(
+      text = "Empty catch block",
+      defaultLevel = Levels.Warning,
+      description = "Finds catch blocks that don't handle caught exceptions.",
+      explanation =
+        "If you use a try/catch block to deal with an exception, you should handle all of the caught exceptions and if for some reason you're throwing another exception in the result, you should include the original exception as the cause."
+    ) {
+
+  import tpd.*
+
+  def inspect(feedback: Feedback[SourcePosition], tree: tpd.Tree)(using ctx: Context): Unit = {
+    val traverser = new InspectionTraverser {
+      def getTreeChildren(tree: Tree): Seq[Tree] = tree match {
+        case Apply(fun, args)               => fun +: args
+        case TypeApply(fun, args)           => fun +: args
+        case Select(qualifier, _)           => Seq(qualifier)
+        case Typed(expr, tpt)               => Seq(expr, tpt)
+        case Block(stats, expr)             => stats :+ expr
+        case If(cond, thenp, elsep)         => Seq(cond, thenp, elsep)
+        case Assign(lhs, rhs)               => Seq(lhs, rhs)
+        case CaseDef(pat, guard, body)      => Seq(pat, guard, body)
+        case Try(block, catches, finalizer) => block +: (catches :+ finalizer)
+        case _                              => Nil
+      }
+
+      def containsMaskingThrow(expectedCauseException: Name, trees: Seq[Tree])(using Context): Boolean = {
+        trees.exists { tree =>
+          val thisResult = tree match {
+            // Check if this is a throw statement that creates a new exception without including the caught exception
+            case t if t.symbol.name.toString == "throw" =>
+              t match {
+                case Apply(_, List(Apply(Select(New(_), _), args))) =>
+                  // Check if args contains the expected exception
+                  !args.exists {
+                    case Ident(i: Name) if i == expectedCauseException => true
+                    case _                                             => false
+                  }
+                case _ => false
+              }
+            case _ => false
+          }
+          thisResult || containsMaskingThrow(expectedCauseException, getTreeChildren(tree))
+        }
+      }
+
+      def checkCatches(defs: List[CaseDef])(using Context): Unit =
+        defs.foreach {
+          case CaseDef(Bind(name, _), _, _) if name.toString == "ignored" || name.toString == "ignore" =>
+          case cdef @ CaseDef(_, _, Literal(c)) if c.value == () && cdef.body.toString == "()" =>
+            feedback.warn(cdef.sourcePos, self, cdef.asSnippet.map(_.take(100)))
+          case cdef @ CaseDef(Bind(caughtException, _), _, subtree)
+              if containsMaskingThrow(caughtException, Seq(subtree)) =>
+            feedback.warn(cdef.sourcePos, self, cdef.asSnippet.map(_.take(100)))
+          case _ =>
+        }
+
+      def traverse(tree: Tree)(using Context): Unit = {
+        tree match {
+          case Try(_, catches, _) =>
+            checkCatches(catches)
+            traverseChildren(tree)
+          case _ => traverseChildren(tree)
+        }
+      }
+    }
+    traverser.traverse(tree)
+  }
+}
